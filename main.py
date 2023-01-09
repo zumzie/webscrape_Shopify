410159b4d4981706f3e043c0d85efa17549e8d35
christian-009 <christian.j.aguero@gmail.com>
2023-01-08 18:16:25 -0800

    Organized data to match bulkstyle endpoint
    next steps:
    - break down data into their own lists; style, images, color, size, etc
    - create bulkstyle endpoint function
    - refactor organizing data function

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import pandas as pd
 import json
 import requests
 import grab_token as grab
-from requests import Session
+from io import StringIO
+from html.parser import HTMLParser
 import csv
 
 url = "https://gstq.com/products.json?limit=250&page=1"
@@ -13,29 +14,62 @@ r = requests.get(url)
 data = r.json()
 product_list = []
 
-for item in data['products']:
-    title = item['title']
-    handle = item['handle']
-    created = item['created_at']
-    description = item['body_html']
-    product_type = item['product_type']
 
-    for image in item['images']:
-        try:
-            imagesrc = image['src']
-        except:
-            imagesrc = 'None'
 
-    for option in item['options']:
+# class html Parser for description of products
+class MLStripper(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.reset()
+        self.strict = False
+        self.convert_charrefs= True
+        self.text = StringIO()
+    def handle_data(self, d):
+        self.text.write(d)
+    def get_data(self):
+        return self.text.getvalue()
 
-        option_name = option['name']
-        option_values = option['values']
+# assign html parser class to variable and feed data into parser
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
 
-    for variant in item['variants']:
-        price = variant['price']
-        sku = variant['sku']
-        available = variant['available']
-        color_option = variant['option2']
+# Pull style data and dump data into json file -> return style data
+def get_styleData():
+    style_request = requests.get(url)
+    style_data = style_request.json()
+    with open('data.json', 'w', encoding='utf-8') as f:
+        json.dump(style_data, f, ensure_ascii=False, indent=4)
+    return style_data
+
+# Organize the style data that was pulled
+def organize_styleData():
+    style_data = get_styleData()
+
+    for item in data["products"]:
+        title = item['title']
+        handle = item['handle']
+        created = item['created_at']
+        description = item['body_html']
+        product_type = item['product_type']
+
+        for image in item['images']:
+            try:
+                imagesrc = image['src']
+            except:
+                imagesrc = 'None'
+
+        for option in item['options']:
+
+            option_name = option['name']
+            option_values = option['values']
+
+        for variant in item['variants']:
+            price = variant['price']
+            sku = variant['sku']
+            available = variant['available']
+            color_option = variant['option2']
 
         product = {
             'title': title,
@@ -48,21 +82,32 @@ for item in data['products']:
             'image': imagesrc,
             'option': option_name,
             'option_values': option_values,
-            'option2': color_option
-            
+            'option2': color_option   
         }
-        
-        product_list.append(product)
-    
-df = pd.DataFrame(product_list)
 
-df.to_csv('testrun2.csv')
-print('test)')
+        style_dict = {
+            "style": [product]
+        }
+            
+        product_list.append(style_dict)
+        for style in style_dict:
+            bulkstyle_dict = {
+                "bulk_styles": product_list
+            }
+    return bulkstyle_dict
+
+print(organize_styleData())
+
+# Dump organized data into json file
+with open('clean_productdata.json', 'w', encoding='utf-8') as f:
+    json.dump(organize_styleData(), f, ensure_ascii=False, indent=4)
 
 
+# joor base url
+base_url = "https://apisandbox.jooraccess.com/v2/"
+url = "style-number/?count=100"
 
-url = "https://apisandbox.jooraccess.com/v2/style-number/?count=100"
-
+# joor class to initialize header
 class JOOR:
     API_TOKEN = grab.token
     def __init__(self):
@@ -83,13 +128,22 @@ header = {
             "authorization": f"Bearer {global_API_TOKEN}"
 }
 
-req_class = requests.get(url, headers=JOOR.get_header)
-req = requests.get(url, headers=header)
 
-print(req_class)
+
+
+
+
+req = requests.get(base_url+url, headers=header)
+
+#print(req_class)
 print("\n\n")
 print(req)
-
+print("\n\n")
+print(req.content)
+print("\n\n")
+print(req.headers)
+print("\n\n")
+print(req.request)
 
 
 
